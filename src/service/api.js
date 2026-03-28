3a845315d1a309b0461d4c9d76dd6e63e9577067
Mounika-satani <mounika@weiteredge.com>
2026-03-29 00:10:27 +0530

    done

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'http://3.236.250.43:8000/api';
-export const API_URL = 'http://localhost:8000/api';
+export const API_URL = 'http://3.236.250.43:8000/api';
+// export const API_URL = 'http://localhost:8000/api';
 
 const originalFetch = window.fetch;
 window.fetch = async function (...args) {
